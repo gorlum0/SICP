c3610243351cfc9c623b7c01b7befaef5ccaa217
gorlum0 <gorlum0@gmail.com>
2011-06-12 03:47:11 +0600

    tabs -> spaces

--- chap1/22.scm
+++ chap1/22.scm
@@ -6,10 +6,10 @@
   (start-prime-test n (runtime)))
 (define (start-prime-test n start-time)
   (if (prime? n)
-	  (begin
-		(display n)
-		(report-prime (- (runtime) start-time)))
-	  0))
+      (begin
+        (display n)
+        (report-prime (- (runtime) start-time)))
+      0))
 (define (report-prime elapsed-time)
   (display " *** ")
   (display-line elapsed-time)
@@ -18,8 +18,8 @@
 ; 3 smallest starting from odd n
 (define (search-for-primes n k)
   (if (> k 0)
-	  (search-for-primes (+ n 2)
-						 (- k (timed-prime-test n)))))
+      (search-for-primes (+ n 2)
+                         (- k (timed-prime-test n)))))
 
 (search-for-primes 1001 3) (newline)
 (search-for-primes 10001 3) (newline)
