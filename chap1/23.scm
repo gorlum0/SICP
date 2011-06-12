46d4a520e4508d0a22b332fe312cb501bbe809de
gorlum0 <gorlum0@gmail.com>
2011-06-12 16:40:26 +0600

    Initial.

---
+++ chap1/23.scm
@@ -0,0 +1,14 @@
+
+(load "display-line.scm")
+(load "smallest-divisor.scm")
+(load "timed-prime-test.scm")
+
+(search-for-primes 1001 3) (newline)
+(search-for-primes 10001 3) (newline)
+(search-for-primes 100001 3) (newline)
+(search-for-primes 1000001 3) (newline)
+(search-for-primes 10000001 3) (newline)
+(search-for-primes 100000001 3) (newline)
+(search-for-primes 1000000001 3) (newline)
+(search-for-primes 10000000001 3) (newline)
+(search-for-primes 100000000001 3) (newline)
