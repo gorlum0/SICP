675e32516890643eee46b7267d5e3200d9b474d2
gorlum0 <gorlum0@gmail.com>
2011-06-12 16:39:38 +0600

    2 smallest-divisor versions

--- chap1/22.scm
+++ chap1/22.scm
@@ -1,25 +1,18 @@
 
 (load "display-line.scm")
-(load "smallest-divisor.scm")
+(load "timed-prime-test.scm")
 
-(define (timed-prime-test n)
-  (start-prime-test n (runtime)))
-(define (start-prime-test n start-time)
-  (if (prime? n)
-      (begin
-        (display n)
-        (report-prime (- (runtime) start-time)))
-      0))
-(define (report-prime elapsed-time)
-  (display " *** ")
-  (display-line elapsed-time)
-  1)
-
-; 3 smallest starting from odd n
-(define (search-for-primes n k)
-  (if (> k 0)
-      (search-for-primes (+ n 2)
-                         (- k (timed-prime-test n)))))
+(define (prime? n)
+  (= n (smallest-divisor n)))
+; we need old smallest-divisor here
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
 
 (search-for-primes 1001 3) (newline)
 (search-for-primes 10001 3) (newline)
@@ -29,3 +22,4 @@
 (search-for-primes 100000001 3) (newline)
 (search-for-primes 1000000001 3) (newline)
 (search-for-primes 10000000001 3) (newline)
+(search-for-primes 100000000001 3) (newline)
