405526248fa6f632e17465b90fb1acafb62f14a7
gorlum0 <gorlum0@gmail.com>
2011-06-18 22:29:48 +0600

    Initia.

---
+++ chap1/31.scm
@@ -0,0 +1,27 @@
+#|(c) gorlum0 [at] gmail.com|#
+(load "../display-line.scm")
+
+(define (product term a next b)
+  (define (iter a result)
+	(if (> a b)
+		result
+		(iter (next a) (* (term a) result))))
+  (iter a 1))
+
+(define (wallis n)
+  (let ((m (* 2.0 n)))
+	(* (/ m (- m 1))
+	   (/ m (+ m 1)))))
+
+;=====
+
+(display-line (product (lambda (x) x)
+					   1
+					   (lambda (x) (+ x 1))
+					   10))
+
+(display-line (* (product wallis
+						  1
+						  (lambda (x) (+ x 1))
+						  100000)
+				 2))
