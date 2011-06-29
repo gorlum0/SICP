1c4aabbdf6725e0174a722fbd0d145497653f3e4
gorlum0 <gorlum0@gmail.com>
2011-06-30 01:02:33 +0600

    Minor.

--- basic.scm
+++ basic.scm
@@ -3,6 +3,8 @@
   (display obj)
   (newline))
 
+;=====
+
 (define (inc x) (+ x 1))
 (define (cube x) (* x x x))
 
@@ -10,4 +12,16 @@
   (/ (+ x y)
 	 2.0))
 
-  
+; n in Z
+(define (pow x n)
+  (if (= n 0)
+	  1
+	  (* x (pow x (- n 1)))))
+
+(define (log2 x)
+  (/ (log x) (log 2)))
+
+;=====
+
+(define (even? x)
+  (= (remainder x 2) 0))
